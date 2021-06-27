978f05b45e2e2f101ebe13b4afb4ee6987b432a2
Samuel Casa <samuel.casa@bluecare.ch>
2021-06-28 01:15:19 +0200

    Add fibonacci endpoint

--- server.py
+++ server.py
@@ -34,5 +34,36 @@ def echo(number):
     return {"data": number}, 200
 
 
+class Fibonacci:
+    def __init__(self):
+        self._call = 0
+        self._first = 0
+        self._second = 1
+
+    def next(self):
+        self._call += 1
+
+        if self._call == 1:
+            return {"call": 1, "value": 0}
+
+        if self._call == 2:
+            return {"call": 2, "value": 1}
+
+        next_value = self._first + self._second
+
+        self._first = self._second
+        self._second = next_value
+
+        return {"call": self._call, "value": next_value}
+
+
+FIBONACCI = Fibonacci()
+
+
+@APP.route("/fibonacci")
+def fibonacci():
+    return FIBONACCI.next(), 200
+
+
 if __name__ == "__main__":
     APP.run(debug=True, host="0.0.0.0", port=8080)
